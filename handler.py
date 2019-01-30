4072e91de5baf0578b9796ed105b74e504168f4d
Scott Brenner <scott@scottbrenner.me>
2019-01-29 21:13:43 -0800

    Initial dump - function working

---
+++ handler.py
@@ -0,0 +1,17 @@
+import logging
+import praw
+import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def run(event, context):
+  reddit = praw.Reddit(user_agent='reddelete (by /u/scottstimo)',
+    client_id=os.environ['REDDIT_CLIENT_ID'],
+    client_secret=os.environ['REDDIT_CLIENT_SECRET'],
+    username=os.environ['REDDIT_USERNAME'],
+    password=os.environ['REDDIT_PASSWORD'])
+  for comment in reddit.redditor(os.environ['REDDIT_USERNAME']).comments.new(limit=None):
+    if (comment.score < int(os.environ['DELETE_THRESHOLD'])):
+      comment.delete()
+      logger.info("Deleted: " + comment.id)
